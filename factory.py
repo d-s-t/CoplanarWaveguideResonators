ed413b9a1957eed952565c5e8848bc7af6931d07
d-s-t <davidst360@gmail.com>
2025-10-22 09:58:28 +0300

    refactor presets to use lists for coupling capacitance values

--- factory.py
+++ factory.py
@@ -18,17 +18,21 @@ Substrates = {
     "effective": EffectiveSubstrate
 }
 
-PRESETS_COUPLING_CAPACITANCE = {
-    'A': 56.4e-15,
-    'B': 48.6e-15,
-    'C': 42.9e-15,
-    'D': 35.4e-15,
-    'E': 26.4e-15,
-    'F': 18.0e-15,
-    'G': 11.3e-15,
-    'H': 3.98e-15,
-    'I': 0.44e-15,
-    'J': 0.38e-15,
-    'K': 0.32e-15,
-    'L': 0.24e-15
-}
+PRESET_NAMES = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'
+]
+
+PRESETS_COUPLING_CAPACITANCE = [
+    56.4e-15,
+    48.6e-15,
+    42.9e-15,
+    35.4e-15,
+    26.4e-15,
+    18.0e-15,
+    11.3e-15,
+    3.98e-15,
+    0.44e-15,
+    0.38e-15,
+    0.32e-15,
+    0.24e-15
+]
